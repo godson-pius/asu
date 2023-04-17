b2bd8138ff75ace2aef7ba3240aace99cf725b69
Godson Pius <godsonazubuike15@gmail.com>
2023-04-17 11:44:11 +0100

    ChaNGED Logo

--- components/header.php
+++ components/header.php
@@ -212,7 +212,7 @@
                     <div class="header-left">
                         <div class="main-logo-box">
                             <a href="index">
-                                <img src="assets/images/resources/logo.png" alt="Awesome Logo" title="ASU Logo" width="170">
+                                <img src="assets/images/resources/logo.png" alt="Awesome Logo" title="ASU Logo" width="90">
                             </a>
                         </div>
                         <div class="header-social-link">
